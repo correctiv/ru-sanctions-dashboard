3db10cf62e983c0bd89176d900e25368d27b694b
Simon Wörpel <simon.woerpel@medienrevolte.de>
2022-02-25 18:17:32 +0100

    Cleanup JS stuff

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '../styles/index.scss';
+import $ from 'jQuery';
 
-ReactDOM.render(
-  <strong>I am the app!</strong>,
-  document.querySelector('[data-app-id="ru-sanctions-dashboard"]')
-);
+const META_URL =
+  'https://correctiv.github.io/ru-sanctions-dashboard/src/data/meta.json';
+
+$.getJSON(META_URL, (d) => {
+  const lastUpdate = new Date(d.last_updated);
+  console.log(lastUpdate);
+});
