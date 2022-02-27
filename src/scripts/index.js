201787e6a863fa98b833224766181d39284fb90f
Simon Wörpel <simon.woerpel@medienrevolte.de>
2022-02-27 15:21:57 +0100

    Add js injection script

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,46 @@ import $ from 'jQuery';
 const META_URL =
   'https://correctiv.github.io/ru-sanctions-dashboard/src/data/meta.json';
 
-$.getJSON(META_URL, (d) => {
-  const lastUpdate = new Date(d.last_updated);
-  console.log(lastUpdate);
+const inject = (id, value) => $(`[data-id="${id}"]`).text(value || 0);
+
+$.getJSON(META_URL, ({ all, recent, last_updated }) => {
+  // last updated
+  const lastUpdate = new Date(last_updated);
+  inject('number9', lastUpdate.toLocaleString());
+
+  // new sanctions
+  inject('number12', recent.sanctions);
+
+  // all sanctions
+  inject('number13', all.sanctions);
+
+  // persons number1
+  inject('number1', recent.Person);
+
+  // companies number2
+  inject('number2', recent.Company);
+
+  // other number3
+  inject('number3', recent.Organization + recent.LegalEntity);
+
+  // vessels number10
+  inject('number10', recent.Vessel);
+
+  // airplanes number11
+  inject('number11', recent.Airplane);
+
+  // USA number4
+  inject('number4', recent.ch);
+
+  // EU number5
+  inject('number5', recent.eu);
+
+  // UK number6
+  inject('number6', recent.gb);
+
+  // UN number7
+  inject('number7', recent.uno);
+
+  // CH number8
+  inject('number8', recent.ch);
 });
